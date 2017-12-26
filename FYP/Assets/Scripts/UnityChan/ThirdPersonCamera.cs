fb07a480c63916f37e76ed35270ddffab69799f1
Fish <fishyu624@gmail.com>
2017-12-26 18:57:52 +0800

    Add attack animation, sword and simple UI

--- FYP/Assets/Scripts/UnityChan/ThirdPersonCamera.cs
+++ FYP/Assets/Scripts/UnityChan/ThirdPersonCamera.cs
@@ -1,17 +1,10 @@
-﻿//
-// Unityちゃん用の三人称カメラ
-// 
-// 2013/06/07 N.Kobyasahi
-//
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-//Existing Script
 public class ThirdPersonCamera : MonoBehaviour
 {
-	public float smooth = 3f;		// カメラモーションのスムーズ化用変数
-	Transform standardPos;			// the usual position for the camera, specified by a transform in the game
-	Transform frontPos;			// Front Camera locater
+	public float smooth = 10f;		// Camera following speed
+	Transform standardPos;			// Camera normal position
 	Transform jumpPos;			// Jump Camera locater
 	
 	// スムーズに繋がない時（クイック切り替え）用のブーリアンフラグ
@@ -22,9 +15,6 @@ public class ThirdPersonCamera : MonoBehaviour
 	{
 		// 各参照の初期化
 		standardPos = GameObject.Find ("CamPos").transform;
-		
-		if(GameObject.Find ("FrontPos"))
-			frontPos = GameObject.Find ("FrontPos").transform;
 
 		if(GameObject.Find ("JumpPos"))
 			jumpPos = GameObject.Find ("JumpPos").transform;
@@ -37,55 +27,26 @@ public class ThirdPersonCamera : MonoBehaviour
 	
 	void FixedUpdate ()	// このカメラ切り替えはFixedUpdate()内でないと正常に動かない
 	{
-		
-		if(Input.GetButton("Fire1"))	// left Ctlr
-		{	
-			// Change Front Camera
-			setCameraPositionFrontView();
-		}
-		
-		else if(Input.GetButton("Fire2"))	//Alt
-		{	
-			// Change Jump Camera
-			setCameraPositionJumpView();
-		}
-		
-		else
-		{	
-			// return the camera to standard position and direction
-			setCameraPositionNormalView();
-		}
-	}
-
-	void setCameraPositionNormalView()
-	{
-		if(bQuickSwitch == false){
-		// the camera to standard position and direction
-						transform.position = Vector3.Lerp(transform.position, standardPos.position, Time.fixedDeltaTime * smooth);	
-						transform.forward = Vector3.Lerp(transform.forward, standardPos.forward, Time.fixedDeltaTime * smooth);
-		}
-		else{
-			// the camera to standard position and direction / Quick Change
-			transform.position = standardPos.position;	
-			transform.forward = standardPos.forward;
-			bQuickSwitch = false;
-		}
-	}
-
-	
-	void setCameraPositionFrontView()
-	{
-		// Change Front Camera
-		bQuickSwitch = true;
-		transform.position = frontPos.position;	
-		transform.forward = frontPos.forward;
+        if (bQuickSwitch == false)
+        {
+            // the camera to standard position and direction
+            transform.position = Vector3.Lerp(transform.position, standardPos.position, Time.fixedDeltaTime * smooth);
+            transform.forward = Vector3.Lerp(transform.forward, standardPos.forward, Time.fixedDeltaTime * smooth);
+        }
+        else
+        {
+            // the camera to standard position and direction / Quick Change
+            transform.position = standardPos.position;
+            transform.forward = standardPos.forward;
+            bQuickSwitch = false;
+        }
 	}
 
 	void setCameraPositionJumpView()
 	{
 		// Change Jump Camera
 		bQuickSwitch = false;
-				transform.position = Vector3.Lerp(transform.position, jumpPos.position, Time.fixedDeltaTime * smooth);	
-				transform.forward = Vector3.Lerp(transform.forward, jumpPos.forward, Time.fixedDeltaTime * smooth);		
+		transform.position = Vector3.Lerp(transform.position, jumpPos.position, Time.fixedDeltaTime * smooth);	
+		transform.forward = Vector3.Lerp(transform.forward, jumpPos.forward, Time.fixedDeltaTime * smooth);		
 	}
 }
